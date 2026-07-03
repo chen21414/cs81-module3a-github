f9baf533a8af36886e09754b525f768a50ea2aac
Mike Chen <CHEN_MIKE_C@STUDENT.SMC.EDU>
2026-07-02 20:30:01 -0700

    Add initial greeting function

--- journal.js
+++ journal.js
@@ -2,3 +2,22 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function max(a, b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+}
+
+function isEven(num) {
+  return num % 2 === 0;
+}
